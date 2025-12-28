94d8546328e7d9eeda1ae60c60d2d8a683db7e38
Smurx <108748190+Smurx@users.noreply.github.com>
2025-12-28 20:00:46 +0100

    Update pageTitle and baseUrl in quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
+    pageTitle: "DND-IT-Azubi",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "<github-user-name>.github.io/<repository-name>",
+    baseUrl: "Smurx.github.io/DND-IT-Azubi-Website",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
